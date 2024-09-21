8b168522c9e346a6a4969448896cde856381fdd3
DannyM9021 <dmoreno.1109@gmail.com>
2024-09-20 18:55:04 -0700

    Check if works

--- autoload.js
+++ autoload.js
@@ -1,6 +1,6 @@
 // live2d_path 参数建议使用绝对路径
 //const live2d_path = "https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
-const live2d_path = "https://fastly.jsdelivr.net/gh/DannyM9021/live2d-widget-project-VIE/";
+const live2d_path = "https://fastly.jsdelivr.net/gh/DannyM9021/live2d-widget-project-VIE@master/";
 //const live2d_path = "/live2d-widget-project-VIE/";
 
 // 封装异步加载资源的方法
--- src/index.js
+++ src/index.js
@@ -149,7 +149,7 @@ function initWidget(config, apiPath) {
         };
     }
     document.body.insertAdjacentHTML("beforeend", `<div id="waifu-toggle">
-            <span>看板娘</span>
+            <span>Open Assistant</span>
         </div>`);
     const toggle = document.getElementById("waifu-toggle");
     toggle.addEventListener("click", () => {
