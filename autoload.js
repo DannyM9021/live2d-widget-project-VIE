a6086a186aec0cb81c9b1bbde7d661e8b26a1bd3
DannyM9021 <dmoreno.1109@gmail.com>
2024-09-30 12:18:44 -0700

    using a new sdk file

--- autoload.js
+++ autoload.js
@@ -1,6 +1,6 @@
 // live2d_path 参数建议使用绝对路径
 //const live2d_path = "https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
-const live2d_path = "https://fastly.jsdelivr.net/gh/DannyM9021/live2d-widget-project-VIE@log1/";
+const live2d_path = "https://fastly.jsdelivr.net/gh/DannyM9021/live2d-widget-project-VIE@new/";
 //const live2d_path = "/live2d-widget-project-VIE/";
 
 // 封装异步加载资源的方法
@@ -29,14 +29,14 @@ function loadExternalResource(url, type) {
 if (screen.width >= 768) {
 	Promise.all([
 		loadExternalResource(live2d_path + "waifu.css", "css"),
-		loadExternalResource(live2d_path + "live2d.min.js", "js"),
+		loadExternalResource(live2d_path + "live2dcubismcore.min.js", "js"),
 		loadExternalResource(live2d_path + "waifu-tips.js", "js")
 	]).then(() => {
 		// 配置选项的具体用法见 README.md
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://fastly.jsdelivr.net/gh/DannyM9021/live2d_api_Project_VIE@new/",
+			cdnPath: "https://fastly.jsdelivr.net/gh/DannyM9021/live2d_api_Project_VIE@new3/",
 			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
 		});
 	});
